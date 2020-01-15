4e2938ae159303284f58c4560ecef328d90962c8
Shawn Bowers <bowers@gonzaga.edu>
2020-01-14 17:49:10 -0800

    starter files

---
+++ stock.cpp
@@ -0,0 +1,28 @@
+
+//----------------------------------------------------------------------
+// Author:
+// Assignment: 1
+//----------------------------------------------------------------------
+
+#include "stock.h"
+
+
+Stock::Stock(std::string the_symbol) : Security(the_symbol)
+{}
+
+
+void Stock::set_purchase_price(double the_purchase_price)
+{
+  // TODO
+}
+
+double Stock::get_purchase_price() const
+{
+  // TODO
+}
+
+double Stock::sell_value() const
+{
+  // TODO
+}
+
